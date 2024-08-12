4642f6c4d0136cd4fcf413b79fae08723d672f53
dydals99 <whdydals8604@gmail.com>
2024-08-12 14:58:47 +0900

    DB 연결 및 Entity 작성 MariaDB 사용

---
+++ SpringJWT/src/main/java/com/edu/springboot/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.edu.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edu.springboot.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer>{
+
+}
