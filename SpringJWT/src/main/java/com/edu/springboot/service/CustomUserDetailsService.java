02847dfb4bab1ef80ea698eef5d9219187d3992b
dydals99 <whdydals8604@gmail.com>
2024-08-14 16:57:10 +0900

    로그인 검증

--- SpringJWT/src/main/java/com/edu/springboot/service/CustomUserDetailsService.java
+++ SpringJWT/src/main/java/com/edu/springboot/service/CustomUserDetailsService.java
@@ -1,5 +1,36 @@
 package com.edu.springboot.service;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.edu.springboot.dto.CustomUserDetails;
+import com.edu.springboot.entity.UserEntity;
+import com.edu.springboot.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				
+		//DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+						
+			//UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
 }
